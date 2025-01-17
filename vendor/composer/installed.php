dfb3f5a9dd014c617d124cff7b1ef672c76b2180
Gustavo Zorzi <gustavozorzi2003@gmail.com>
2025-01-17 17:58:43 -0300

    Update Composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'zorzi23/forge_script',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a889b85ebb8f1efe81e4292fb1b37dcb2851b311',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'zorzi23/forge_script' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a889b85ebb8f1efe81e4292fb1b37dcb2851b311',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -22,10 +22,12 @@
         'zorzi23/grammi_core' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9a1abc9caa82a9d75011f6d802d9a02d468c65fa',
+            'reference' => '98cf2453b4df9b55bcf9186f5043fb9cad6392ef',
             'type' => 'library',
             'install_path' => __DIR__ . '/../zorzi23/grammi_core',
-            'aliases' => array(),
+            'aliases' => array(
+                0 => '9999999-dev',
+            ),
             'dev_requirement' => false,
         ),
     ),
